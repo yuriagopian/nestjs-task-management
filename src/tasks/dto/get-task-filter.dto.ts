e9c01e810152464d1b890d19f91945bf4a93b10a
yuriagopian <yuri.agopian@hotmail.com>
2023-01-26 18:33:22 -0300

    chore:  add error treatment

--- src/tasks/dto/get-task-filter.dto.ts
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -1,10 +1,12 @@
-import { IsString, IsNotEmpty, IsEnum } from 'class-validator';
+import { IsString, IsNotEmpty, IsEnum, IsOptional } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
   @IsEnum(TaskStatus)
   status: TaskStatus;
 
+  @IsOptional()
   @IsNotEmpty()
   @IsString()
   search: string;
