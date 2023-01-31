bbb15b9e210022606eafdb1128714ebc80435d4e
yuriagopian <yuri.agopian@hotmail.com>
2023-01-31 13:56:57 -0300

    refactor:  task service  by using repository typeorm

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -22,7 +22,7 @@ export class TaskRepository extends Repository<Task> {
 
     if (search) {
       query.andWhere(
-        'task.title  LIKE :search OR task.description LIKE :search',
+        'LOWER(task.title)  LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
         { search: `%${search}%` },
       );
     }
