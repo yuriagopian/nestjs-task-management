041a0414deba224ea506b6afe2c153679e5700ee
yuriagopian <yuri.agopian@hotmail.com>
2023-01-31 13:53:32 -0300

    refactor:  task service  by using repository typeorm

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -12,7 +12,21 @@ export class TaskRepository extends Repository<Task> {
   }
 
   async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { search, status } = filterDto;
+
     const query = this.createQueryBuilder('task');
+
+    if (status) {
+      query.andWhere('task.status= :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        'task.title  LIKE :search OR task.description Line :search',
+        { search: `%${search}%` },
+      );
+    }
+
     const tasks = await query.getMany();
     return tasks;
   }
