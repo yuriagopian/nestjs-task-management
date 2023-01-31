265e8e749dc416f602bc5c5062ffd1cc68f303ec
yuriagopian <yuri.agopian@hotmail.com>
2023-01-31 14:54:38 -0300

    feat: auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersRepository } from './users.repository';
+import { User } from './user.entity';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [AuthService, UsersRepository],
+  controllers: [AuthController],
 })
 export class AuthModule {}
