f7484dbabb0ac2d9acdbdd818337dda8682276e0
yuriagopian <yuri.agopian@hotmail.com>
2023-02-01 14:47:42 -0300

    chore: Implements one to many ralation

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -24,7 +24,7 @@ export class TaskRepository extends Repository<Task> {
 
     if (search) {
       query.andWhere(
-        'LOWER(task.title)  LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        '(LOWER(task.title)  LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
         { search: `%${search}%` },
       );
     }
