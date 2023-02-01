fd28d5ded84bf23974a78d7a59f7127a86bb540b
yuriagopian <yuri.agopian@hotmail.com>
2023-02-01 11:54:54 -0300

    chore: sign in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,13 +7,11 @@ export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('/signup')
   signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
-    // console.log(authCredentialsDto);
     return this.authService.signUp(authCredentialsDto);
   }
 
   @Post('/signin')
   signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
-    // console.log(authCredentialsDto);
     return this.authService.signIn(authCredentialsDto);
   }
 }
