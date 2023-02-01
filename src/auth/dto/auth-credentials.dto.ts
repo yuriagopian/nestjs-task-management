b41bd762a25b0e09648a799c8fe538e4db1702c5
yuriagopian <yuri.agopian@hotmail.com>
2023-02-01 11:10:15 -0300

    chore: dto validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,13 +1,29 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class AuthCredentialsDto {
   @IsString()
   @IsNotEmpty()
-  @MinLength(8)
+  @MinLength(4)
   @MaxLength(20)
   username: string;
 
   @IsString()
   @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(32)
+  /**  Strong Password Validation
+    Passwords will contain at least 1 upper case letter
+    Passwords will contain at least 1 lower case letter
+    Passwords will contain at least 1 number or special character
+    There is no length validation (min, max) in this regex!
+    Regular expression for JavaScript:
+  */
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/)
   password: string;
 }
