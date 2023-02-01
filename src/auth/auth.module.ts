162014055363ae6791d0c1e8fd53d6e35ac85cbd
yuriagopian <yuri.agopian@hotmail.com>
2023-02-01 11:58:12 -0300

    chore: sign in controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,22 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
 import { User } from './user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [AuthService, UsersRepository],
   controllers: [AuthController],
 })
