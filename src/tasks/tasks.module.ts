4b6f68cc644acff3c357b4081ed011359a3567b3
yuriagopian <yuri.agopian@hotmail.com>
2023-02-01 17:52:10 -0300

    chore: config envs

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { Task } from './task.entity';
@@ -7,7 +8,7 @@ import { TaskRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task, AuthModule])],
+  imports: [ConfigModule, TypeOrmModule.forFeature([Task, AuthModule])],
   controllers: [TasksController],
   providers: [TasksService, TaskRepository],
 })
