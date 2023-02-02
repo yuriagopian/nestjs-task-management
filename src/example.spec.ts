926b49d7511a4624c898a258af62f70603d16508
yuriagopian <yuri.agopian@hotmail.com>
2023-02-02 10:40:07 -0300

    test: test example

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,4 +1,4 @@
-function addNumber(num1, num2) {
+function addNumbers(num1, num2) {
   return num1 + num2;
 }
 
@@ -6,5 +6,11 @@ describe('Example test', () => {
   it('should equals true', () => {
     expect(true).toEqual(true);
     expect('Yuri').toEqual('Yuri');
+  });
+});
+
+describe('addNumbers', () => {
+  it('adds two numbers', () => {
+    expect(addNumbers(2, 2)).toEqual(4);
   });
 });
